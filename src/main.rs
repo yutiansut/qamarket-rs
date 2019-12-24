b177d5e1d3654decc3b3abca7ce973b1a399f400
yutiansut <yutiansut@qq.com>
2019-12-24 08:50:31 +0800

    commit for 0.3: 行情分发 1min/5min/15min/30min/60min

--- src/main.rs
+++ src/main.rs
@@ -150,7 +150,7 @@ pub struct QASeries{
 fn main() {
     // let code = "au2002".to_string();
     let subscribe_list =  vec![ "au2006", "a2005", "ag2002", "al2002", "b2002",
-    "bb1912", "bu2006", "c2005", "cu2002", "cs2001","eg2005", "fb2005", "fu2005", "hc2001",
+    "bb2003", "bu2006", "c2005", "cu2002", "cs2001","eg2005", "fb2005", "fu2005", "hc2001",
     "j2005", "i2005", "jd2005", "jm2001", "l2005", "m2005", "ni2002", "p2005", "pb2001",
     "pp2005", "rb2005", "ru2005", "sc2002", "sn2005", "sp2005", "v2005", "y2005"];
     let sub_iter= subscribe_list.iter().cloned();
